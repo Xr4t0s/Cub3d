524aa720a18046f215985664bb086f6f785e167b
Xr4t0s <nicolastadros.maillol@gmail.com>
2025-09-27 16:49:12 +0200

    update

--- src/map/parse_file.c
+++ src/map/parse_file.c
@@ -6,7 +6,7 @@
 /*   By: nitadros <nitadros@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 02:09:52 by nitadros          #+#    #+#             */
-/*   Updated: 2025/09/27 16:23:50 by nitadros         ###   ########.fr       */
+/*   Updated: 2025/09/27 16:49:07 by nitadros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 static void	parse_param_utils(t_data *d, char *trimed, char **param)
 {
-	if (!ft_strncmp(param[0], "NO", 2) && ft_strlen(param[0]) == 2 && check_img(trimed, d))
+	if (!ft_strncmp(param[0], "NO", 2) && ft_strlen(param[0]) == 2
+		&& check_img(trimed, d))
 		handle_no_so(d, trimed, 1);
-	else if (!ft_strncmp(param[0], "SO", 2) && ft_strlen(param[0]) == 2 && check_img(trimed, d))
+	else if (!ft_strncmp(param[0], "SO", 2) && ft_strlen(param[0]) == 2
+		&& check_img(trimed, d))
 		handle_no_so(d, trimed, 2);
-	else if (!ft_strncmp(param[0], "WE", 2) && ft_strlen(param[0]) == 2 && check_img(trimed, d))
+	else if (!ft_strncmp(param[0], "WE", 2) && ft_strlen(param[0]) == 2
+		&& check_img(trimed, d))
 		handle_we_ea(d, trimed, 1);
-	else if (!ft_strncmp(param[0], "EA", 2) && ft_strlen(param[0]) == 2 && check_img(trimed, d))
+	else if (!ft_strncmp(param[0], "EA", 2) && ft_strlen(param[0]) == 2
+		&& check_img(trimed, d))
 		handle_we_ea(d, trimed, 2);
 	fulfill_fc(param, d);
 }
